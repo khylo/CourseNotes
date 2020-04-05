e6c96fd18c441337940d6e899960ab854186d9a7
khylo <keith_hyland@yahoo.com>
2020-04-05 02:45:11 +0100

    small updates

--- GuessGame.py
+++ GuessGame.py
@@ -4,21 +4,29 @@ class Game:
     min=0
     guesses = 0
     target=0
+    solved=False
 
     def start(self, max=100, min=0):
         self.max=max
         self.min=min
         self.guesses=0
         self.target = random.randrange(min,max,1)
+        self.solved=False
         return "Guess a number between "+str(min)+" and "+str(max)
 
     def quit(self):
-        return "Sorry to see you go. THe number was "+str(self.target)
+        if(self.solved):
+            return "You solved it. It took you "+str(self.guesses)+" guess. Would you like to play again?"
+        print("Not  Solved")
+        return "Sorry to see you go. The number was "+str(self.target)
 
     def guess(self, guessed):
+        if(self.solved):
+            return self.quit()
         self.guesses = self.guesses +1
         if (guessed == self.target):
-            return "You got it. It took you "+str(self.guesses)+" guess."
+            self.solved=True
+            return self.quit()
         if (guessed < self.target):
             return "Try a higher number"
         return "Try a lower number"
@@ -26,5 +34,7 @@ class Game:
 
 g = Game()
 print (g.start(6,5))
-print ("Guessing 5 "+g.guess(5))
+print ("Guessing 5 ")
+print (g.guess(5))
+print (g.guess(5))
 print (g.quit())
