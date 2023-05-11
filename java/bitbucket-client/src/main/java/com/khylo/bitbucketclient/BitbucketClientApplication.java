be8d52e21eeeb166eab191f424c368c6b8288a82
khylo <keith_hyland@yahoo.com>
2023-05-11 19:20:49 +0100

    Adding bitbucket client generater

---
+++ java/bitbucket-client/src/main/java/com/khylo/bitbucketclient/BitbucketClientApplication.java
@@ -0,0 +1,13 @@
+package com.khylo.bitbucketclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbucketClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbucketClientApplication.class, args);
+	}
+
+}
