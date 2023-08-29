7f2336008caf6c0ab1e96eac0393c0141c21a52b
khylo <keith_hyland@yahoo.com>
2023-08-29 08:25:22 +0100

    Python

---
+++ python/numPan.py
@@ -0,0 +1,12 @@
+# pip install numpy
+# Taken from https://www.kaggle.com/code/legendadnan/numpy-tutorial-for-beginners-data-science
+# Once you've installed NumPy you can import it as a library:
+import numpy as np
+my_list = [1,2,3]
+np.array(my_list)
+
+# linspace.. makes list of 3 between range
+np.linspace(0,10,3)
+# Identity matrix
+np.eye(4)  # 4*4
+
